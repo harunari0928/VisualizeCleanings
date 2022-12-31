e9a69c9b3ad74c26b9fedc4eaf4e075e22304be1
Seibi0928 <ryo13579978@gmail.com>
2022-12-31 09:42:23 +0000

    実績ページの実装

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,18 @@
 ﻿import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Top } from './pages/Top';
-
+import { Achievements } from './pages/Achievements';
 
 const container = document.getElementById('root');
 if (!container) {
     throw new Error('The element (id=root) does not exist.');
 }
-createRoot(container).render(<Top />);
+createRoot(container).render(
+    <Router>
+        <Routes>
+            <Route path="/" element={<Top />} />
+            <Route path="/achievements" element={<Achievements />} />
+        </Routes>
+    </Router>
+);
---
+++ src/app/pages/Achievements.tsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+import AddIcon from '@mui/icons-material/Add';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Grid from '@mui/material/Grid';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const data = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+export function Achievements() {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Grid container>
+        <Grid item md={4} />
+        <Grid item md={4} xs={12}>
+          <Doughnut data={data} />
+        </Grid>
+        <Grid item md={4} />
+      </Grid>
+      <Grid container>
+        <Grid item xs={0} md={2} />
+        <Grid item xs={3}>
+          <Button variant="contained" sx={{ borderRadius: 28 }} onClick={handleClickOpen}>
+            <AddIcon />登録
+          </Button>
+        </Grid>
+        <Grid item xs={8} />
+      </Grid>
+      <Dialog
+        fullWidth={true}
+        maxWidth="md"
+        open={open}
+        onClose={handleClose}
+      >
+        <DialogTitle>掃除実績登録</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            あなたが実施した掃除の記録を登録します。
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>登録</Button>
+          <Button onClick={handleClose} autoFocus>
+            閉じる
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>);
+}
