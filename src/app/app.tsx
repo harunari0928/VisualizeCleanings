c2bcd9bdc3a7a106a8104cad8a4070b15054804f
Seibi0928 <nanteneus@hotmail.co.jp>
2022-12-31 09:13:48 +0000

    add top menue page.

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,10 @@
 ﻿import React from 'react';
 import { createRoot } from 'react-dom/client';
-import '../stylesheet/app.scss';
-import { Base } from './Base';
+import { Top } from './pages/Top';
 
 
 const container = document.getElementById('root');
 if (!container) {
     throw new Error('The element (id=root) does not exist.');
 }
-createRoot(container).render(<Base />);
+createRoot(container).render(<Top />);
---
+++ src/app/pages/Top.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import BarChartIcon from '@mui/icons-material/BarChart';
+import CleaningServicesIcon from '@mui/icons-material/CleaningServices';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import { styled } from "@mui/material";
+
+const TopGrid = styled(Grid)(({ }) => ({
+  marginTop: '40vh',
+}));
+const MenuBottn = styled(Button)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export function Top() {
+  return (
+    <TopGrid container spacing={2}>
+      <Grid item xs={4} />
+      <Grid item xs={4}>
+          <Stack
+            justifyContent="center"
+            direction={{ xs: 'column', sm: 'row' }}
+            spacing={{ xs: 1, sm: 2, md: 4 }}>
+            <MenuBottn variant="contained">掃除実績<BarChartIcon /></MenuBottn>
+            <MenuBottn variant="contained">掃除内容<CleaningServicesIcon /></MenuBottn>
+          </Stack>
+      </Grid>
+      <Grid item xs={4} />
+    </TopGrid>);
+}
